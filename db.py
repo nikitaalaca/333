006cc297d388935c5cbf8e3ba5c86efa79066111
nikitaalaca <egorkin.n58@gmail.com>
2025-07-03 00:30:02 +0300

    db.py

--- db.py
+++ db.py
@@ -1,115 +1,100 @@
 import json
 from datetime import datetime, timedelta
-import os
 
 DB_FILE = "storage.json"
+V2RAY_FILE = "users.json"
 
-def load_db():
-    if not os.path.exists(DB_FILE):
-        with open(DB_FILE, "w") as f:
-            json.dump({"users": {}, "admins": [1467435264], "moderators": []}, f)
-    with open(DB_FILE, "r") as f:
-        return json.load(f)
+def load_data():
+    try:
+        with open(DB_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
 
-def save_db(data):
+def save_data(data):
     with open(DB_FILE, "w") as f:
         json.dump(data, f, indent=4)
 
+def load_keys():
+    try:
+        with open(V2RAY_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save_keys(data):
+    with open(V2RAY_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
 def get_subscription(user_id):
-    data = load_db()
-    user = data["users"].get(str(user_id))
-    if user and "subscription_until" in user:
-        return datetime.strptime(user["subscription_until"], "%Y-%m-%d %H:%M:%S")
+    data = load_data()
+    ts = data.get(user_id, {}).get("sub_until")
+    if ts:
+        return datetime.strptime(ts, "%Y-%m-%d %H:%M:%S")
     return None
 
 def set_subscription(user_id, username, days, trial=False):
-    data = load_db()
-    user_id = str(user_id)
-    now = datetime.utcnow()
-    expire_time = now + timedelta(days=days)
-
-    if user_id not in data["users"]:
-        data["users"][user_id] = {"username": username, "used_trial": False}
-
-    data["users"][user_id]["subscription_until"] = expire_time.strftime("%Y-%m-%d %H:%M:%S")
-    if trial:
-        data["users"][user_id]["used_trial"] = True
-
-    save_db(data)
-    return expire_time
+    data = load_data()
+    expire = datetime.utcnow() + timedelta(days=days)
+    data[user_id] = {
+        "username": username,
+        "sub_until": expire.strftime("%Y-%m-%d %H:%M:%S"),
+        "trial": trial
+    }
+    save_data(data)
+    return expire
 
 def has_used_trial(user_id):
-    data = load_db()
-    user = data["users"].get(str(user_id))
-    return user.get("used_trial", False) if user else False
+    data = load_data()
+    return data.get(user_id, {}).get("trial", False)
 
 def deactivate_expired_users():
-    data = load_db()
+    data = load_data()
     now = datetime.utcnow()
-    for user in data["users"].values():
-        if "subscription_until" in user:
-            sub_time = datetime.strptime(user["subscription_until"], "%Y-%m-%d %H:%M:%S")
-            if sub_time < now:
-                user["subscription_until"] = None
-    save_db(data)
-
-def set_key(user_id, key):
-    data = load_db()
-    user_id = str(user_id)
-    if user_id in data["users"]:
-        data["users"][user_id]["key"] = key
-        save_db(data)
-
-def get_key(user_id):
-    data = load_db()
-    return data["users"].get(str(user_id), {}).get("key")
-
-def is_admin(user_id):
-    data = load_db()
-    return user_id in data.get("admins", [])
-
-def is_moderator(user_id):
-    data = load_db()
-    return user_id in data.get("moderators", [])
-
-def add_admin(user_id):
-    data = load_db()
-    if user_id not in data["admins"]:
-        data["admins"].append(user_id)
-        save_db(data)
-
-def remove_admin(user_id):
-    data = load_db()
-    if user_id in data["admins"] and user_id != 1467435264:  # Главного админа нельзя удалить
-        data["admins"].remove(user_id)
-        save_db(data)
-
-def add_moderator(user_id):
-    data = load_db()
-    if user_id not in data["moderators"]:
-        data["moderators"].append(user_id)
-        save_db(data)
-
-def remove_moderator(user_id):
-    data = load_db()
-    if user_id in data["moderators"]:
-        data["moderators"].remove(user_id)
-        save_db(data)
+    for uid in list(data.keys()):
+        sub_until = datetime.strptime(data[uid]["sub_until"], "%Y-%m-%d %H:%M:%S")
+        if sub_until < now:
+            del data[uid]
+    save_data(data)
 
 def get_all_users():
-    data = load_db()
-    return data["users"]
+    return load_data()
 
 def delete_user(user_id):
-    data = load_db()
-    if str(user_id) in data["users"]:
-        del data["users"][str(user_id)]
-        save_db(data)
+    data = load_data()
+    keys = load_keys()
+    if user_id in data:
+        del data[user_id]
+    if user_id in keys:
+        del keys[user_id]
+    save_data(data)
+    save_keys(keys)
 
+def update_v2ray_key(user_id, key):
+    keys = load_keys()
+    keys[user_id] = key
+    save_keys(keys)
 
-def update_v2ray_key(user_id, new_key):
-    data = load_db()
-    user_id = str(user_id)
-    if user_id in data["users"]:
-        data["users"][user_id]["key"] = new_key
-        save_db(data)
+def get_v2ray_key(user_id):
+    keys = load_keys()
+    return keys.get(user_id)
+
+def is_admin(user_id):
+    return str(user_id) in load_data().get("admins", [])
+
+def is_moderator(user_id):
+    return str(user_id) in load_data().get("moderators", [])
+
+def add_admin(user_id):
+    data = load_data()
+    if "admins" not in data:
+        data["admins"] = []
+    if user_id not in data["admins"]:
+        data["admins"].append(user_id)
+    save_data(data)
+
+def remove_admin(user_id):
+    data = load_data()
+    if "admins" in data and user_id in data["admins"]:
+        data["admins"].remove(user_id)
+    save_data(data)
